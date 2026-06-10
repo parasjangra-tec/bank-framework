7b11b8ebfe87eee4627d1dd835e675a6ebc49185
parasjangra-sf <paras.jangra@sourcefuse.com>
2026-06-10 11:47:18 +0530

    Remove practice failure test

--- tests/smoke/failurePractice.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Intentional failure', async ({ page }) => {
-
-  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
-
-  await expect(page).toHaveTitle('Wrong Title');
-
-});
