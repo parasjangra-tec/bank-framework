10e4ecb35ac3924c8896dc96b82f4ed22af025d8
parasjangra-sf <paras.jangra@sourcefuse.com>
2026-06-11 11:28:19 +0530

    Fixed WebKit test failures and dashboard locator

--- tests/regression/dashboard.spec.js
+++ tests/regression/dashboard.spec.js
@@ -4,6 +4,18 @@ test('Verify Dashboard Page', async ({ page }) => {
 
   await page.goto('https://parabank.parasoft.com/parabank/index.htm');
 
-  await expect(page).toHaveTitle(/ParaBank/);
+  await page.locator('input[name="username"]').fill('john');
+
+  await page.locator('input[name="password"]').fill('demo');
+
+  await page.locator('input[value="Log In"]').click();
+
+  await expect(page).toHaveURL(/overview.htm/);
+
+  await expect(
+    page.getByRole('heading', {
+      name: 'Accounts Overview'
+    })
+  ).toBeVisible();
 
 });
